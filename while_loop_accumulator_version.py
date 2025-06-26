fd4ca0caec64c4fd87b47d5f0514874571dc082d
jocelyneh04 <jlv9895@nyu.edu>
2025-06-26 10:22:35 -0400

    modified code

---
+++ while_loop_accumulator_version.py
@@ -0,0 +1,25 @@
+def get_starting_number():
+    while True:
+        user_input = input("How many bottles of beer on the wall? ")
+        if user_input.isdigit():
+            number = int(user_input)
+            if number >= 1:
+                return number
+
+def sing(start):
+    bottles = start
+    while bottles > 0:
+        if bottles > 1:
+            next_bottles = bottles - 1
+            bottle_word = "bottles"
+            next_word = "bottles"
+            if next_bottles == 1:
+                next_word = "bottle"
+            print(f"{bottles} {bottle_word} of beer on the wall, {bottles} {bottle_word} of beer.")
+            print(f"Take one down, pass it around, {next_bottles} {next_word} of beer on the wall.")
+            print()
+        else:
+            print("1 bottle of beer on the wall, 1 bottle of beer.")
+            print("Take it down, pass it around, no more bottles of beer on the wall!")
+            print()
+        bottles = bottles - 1
